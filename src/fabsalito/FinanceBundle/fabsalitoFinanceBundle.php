425c4280200fae6d7b77bff2d871fc2cc3616f3f
fabsal <fabricio.salinas@gmail.com>
2013-03-13 11:52:59 -0600

    Deleted local FinanceBundle and GeoBundle.-

--- src/fabsalito/FinanceBundle/fabsalitoFinanceBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace fabsalito\FinanceBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class fabsalitoFinanceBundle extends Bundle
-{
-}
--- src/fabsalito/GeoBundle/fabsalitoGeoBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace fabsalito\GeoBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class fabsalitoGeoBundle extends Bundle
-{
-}
